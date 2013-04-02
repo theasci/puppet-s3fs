ec858f35784c75830573dd11a84f2e7975f29c79
John Jarvis <jarv@edx.org>
2013-04-02 15:11:37 -0400

    removing libxml2-dev dependencies

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -4,7 +4,6 @@ class s3fs::dependencies {
   if ! defined(Package['libfuse-dev'])          { package { 'libfuse-dev':          ensure => installed } }
   if ! defined(Package['fuse-utils'])           { package { 'fuse-utils':           ensure => installed } }
   if ! defined(Package['libcurl4-openssl-dev']) { package { 'libcurl4-openssl-dev': ensure => installed } }
-  if ! defined(Package['libxml2-dev'])          { package { 'libxml2-dev':          ensure => installed } }
   if ! defined(Package['mime-support'])         { package { 'mime-support':         ensure => installed } }
 
 }
